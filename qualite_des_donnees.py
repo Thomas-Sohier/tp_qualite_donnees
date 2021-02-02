3cb9687883cb433d24b46e4fc37ef07271477286
CLOAREC Yann <yann.cloarec@epsi.fr>
2021-02-02 12:26:23 +0100

    Table comparatif + score

--- qualite_des_donnees.py
+++ qualite_des_donnees.py
@@ -14,28 +14,54 @@ import statistics
 # Class
 #
 class SnaptoCursor(object):
-    def __init__(self, ax, x, y):
-        self.ax = ax
-        self.ly = ax.axvline(color='k', alpha=0.2)
-        self.marker, = ax.plot([0],[0], marker="o", color="crimson", zorder=3) 
-        self.x = x
-        self.y = y
-        self.txt = ax.text(0.7, 0.9, '')
+  def __init__(self, ax, x, y):
+      self.ax = ax
+      self.ly = ax.axvline(color='k', alpha=0.2)
+      self.marker, = ax.plot([0],[0], marker="o", color="crimson", zorder=3) 
+      self.x = x
+      self.y = y
+      self.txt = ax.text(0.7, 0.9, '')
 
-    def mouse_move(self, event):
-        if not event.inaxes: return
-        x, y = event.xdata, event.ydata
-        indx = np.searchsorted(self.x, [x])[0]
-        x = self.x[indx]
-        y = self.y[indx]
-        self.ly.set_xdata(x)
-        self.marker.set_data([x],[y])
-        text = '%1.2f°C (jour %1d)' % (y, x)
-        avg = getAverageXDays(self,event.xdata,event.ydata,30)
-        self.txt.set_text(text + ' \n'+'%1.2f°C (moyenne 30d)' % (avg))
-        self.txt.set_position((x,y))
-        self.txt.set_position((x,y))
-        self.ax.figure.canvas.draw_idle()
+  def mouse_move(self, event):
+      if not event.inaxes: return
+      x, y = event.xdata, event.ydata
+      indx = np.searchsorted(self.x, [x])[0]
+      x = self.x[indx]
+      y = self.y[indx]
+      self.ly.set_xdata(x)
+      self.marker.set_data([x],[y])
+      text = '%1.2f°C (jour %1d)' % (y, x)
+      avg = getAverageXDays(self,event.xdata,event.ydata,30)
+      self.txt.set_text(text + ' \n'+'%1.2f°C (moyenne 30d)' % (avg))
+      self.txt.set_position((x,y))
+      self.txt.set_position((x,y))
+      self.ax.figure.canvas.draw_idle()
+
+class SnapToCursorCompare(object):
+
+  def __init__(self, ax, x, y, color, i, j):
+    self.ax = ax
+    self.ly = ax.axvline(color='k', alpha=0.2)  # the vert line
+    self.marker, = ax.plot([0],[0], marker="o", color=color, zorder=3)
+    self.color = color
+    self.x = x
+    self.y = y
+    self.i = i
+    self.j = j
+    self.txt = ax.text(0.7, 0.9, '')
+
+  def mouse_move(self, event):
+      if not event.inaxes: return
+      x, y = event.xdata, event.ydata
+      indx = np.searchsorted(self.x, [x])[0]
+      x = self.x[indx+1]
+      y = self.y[indx]
+      self.ly.set_xdata(x)
+      self.marker.set_data([x],[y])
+      self.txt.set_text('%1.2f°C (jour %1d)' % (y, x))
+      self.txt.set_color(self.color)
+      self.txt.set_position((0,self.j-(4*self.i)-8))
+      self.ax.figure.canvas.draw_idle()
 #
 # Fontions
 #
@@ -77,20 +103,6 @@ def setValeursMoisErreur(df_erreur):
     values_by_month_error.append(month_temperature)
   return values_by_month_error
 
-def setValeursMoissavukoski(savukoski):
-  values_by_month_savukoski=[]
-  current_month=1
-  month_temperature_savukoski=[]
-  for row in range(1, 365):
-    if(savukoski.iloc[row, 1] != current_month):
-      current_month=current_month+1
-      values_by_month_savukoski.append(month_temperature_savukoski.copy())
-      month_temperature_savukoski=[]
-    temperature_value_savukoski = savukoski.iloc[row, 5]
-    month_temperature_savukoski.append(temperature_value_savukoski.copy())
-  values_by_month_savukoski.append(month_temperature_savukoski.copy())
-  return values_by_month_savukoski
-
 def getValeursMoisErreurCorrection(year):
   values_by_month_ok = []
   for indexMonth,month in enumerate(year,start=0):
@@ -112,6 +124,32 @@ def getValeursMoisErreurCorrection(year):
           year[indexMonth][indexDay]=np.average([month[indexDay-1],month[indexDay+1]])
   values_by_month_ok=year.copy()
   return values_by_month_ok
+
+def getValeursMoissavukoski(savukoski):
+  values_by_month_savukoski=[]
+  current_month=1
+  month_temperature_savukoski=[]
+  for row in range(1, 365):
+    if(savukoski.iloc[row, 1] != current_month):
+      current_month=current_month+1
+      values_by_month_savukoski.append(month_temperature_savukoski.copy())
+      month_temperature_savukoski=[]
+    temperature_value_savukoski = savukoski.iloc[row, 5]
+    month_temperature_savukoski.append(temperature_value_savukoski.copy())
+  values_by_month_savukoski.append(month_temperature_savukoski.copy())
+  return values_by_month_savukoski
+
+def getValeursMoisHelsinki(dataset):
+  values_by_month_dataset=[]
+  for column in range(0, 12):
+    month_temperature_dataset=[]
+    for row in range(0, 31):
+      temperature_value_dataset = (dataset.iloc[row, column] - 32) * 5/9
+      if(not np.isnan(temperature_value_dataset)):
+        month_temperature_dataset.append(temperature_value_dataset)
+    values_by_month_dataset.append(month_temperature_dataset)
+  return values_by_month_dataset
+
 
 def getValeurApres(value_error, rowIndex, columnIndex):
     nextValue = value_error.iloc[(rowIndex + 1), columnIndex]
@@ -158,6 +196,22 @@ def getMaxAnnee(dataset):
   max_year= max(getMaxParMois(dataset))
   return max_year
 
+def getScore(dataset1, dataset2):
+  moy_data1=getMoy(dataset1)
+  moy_data2=getMoy(dataset2)
+  et_data1=getEcartType(dataset1)
+  et_data2=getEcartType(dataset2)
+  score_mois=[]
+  score_annee=0
+  score_retour=[]
+  for m in range(0,12):
+    score=abs(moy_data1[m]-moy_data2[m])+abs(et_data1[m]-et_data2[m])
+    score_annee=score_annee + score
+    score_mois.append(score)
+  score_retour.append(score_annee)
+  score_retour.append(score_mois)
+  return score_retour
+
 def getCourbe(mois,libelle):
   plot = plt.figure(libelle)
   plt.plot(mois)
@@ -171,7 +225,6 @@ def getCourbeAnnee(dataset):
   for d in dataset:
     lenD=lenD+len(d)
   t = np.arange(0, lenD, 1)
-  s = np.sin(2 * 2 * np.pi * t)
   df_temp = pd.DataFrame(columns = ['Température'])
   for column in dataset:
       for value in column:
@@ -182,6 +235,36 @@ def getCourbeAnnee(dataset):
   ax.set(title='Année')
   snap_cursor = SnaptoCursor(ax, t, flatten(dataset))
   fig.canvas.mpl_connect('motion_notify_event', snap_cursor.mouse_move)
+  fig.canvas.set_window_title("Année")
+  plt.show()
+
+def getAllCourbesAnnee():
+  fig, ax = plt.subplots(figsize=(15,7))
+  datas=[]
+  datas.append(flatten(values_by_month))
+  datas.append(flatten(data_helsinki))
+  datas.append(flatten(data_savukoski))
+  nom_fichiers=["Propre","Helsinki","Savukoski"]
+  plt.subplots_adjust(bottom=0.2)
+  
+  plt.title("Comparatif année")
+  plt.xlabel("Jours")
+  plt.ylabel("Degré")
+  
+  min_temp = -20
+  max_temp = 30
+
+  plt.axis([1, 366, min_temp-10, max_temp+10])
+  plt.grid(True)
+
+  cursors = []
+  for i, data in enumerate(datas):
+      t = np.arange(0, 365, 1)
+      plot = plt.plot(data, label=nom_fichiers[i])
+      cursors.append(SnapToCursorCompare(ax, t, data, plot[0].get_color(), i, min_temp-10))
+      plt.connect('motion_notify_event', cursors[i].mouse_move)
+
+  plt.legend()
   plt.show()
 
 def affichageMois(canvas,fenetre,dataset,yPos):
@@ -196,35 +279,44 @@ def affichageMois(canvas,fenetre,dataset,yPos):
     canvas.create_text(280+(m*130),140+yPos,fill="black",font="Times 13",text="Min : "+str("{:.2f}".format(minMois[m])))
     canvas.create_text(280+(m*130),160+yPos,fill="black",font="Times 13",text="Max : "+str("{:.2f}".format(maxMois[m])))
     canvas.create_text(280+(m*130),180+yPos,fill="black",font="Times 13",text="Ecart-type : "+str("{:.2f}".format(ecartType[m])))
+    canvas.create_text(280+(m*130),200+yPos,fill="black",font="Times 13",text="Score : "+str("{:.2f}".format(getScore(values_by_month,dataset)[1][m])))
     b = Button(fenetre, text="Graphique", width=10, command=partial(getCourbe,dataset[m],mois[m]),background="white",foreground="black",activebackground="grey",activeforeground="black")
-    b.place(x=230+(m*130), y=200+yPos, anchor="nw", width=100, height=30)
+    b.place(x=230+(m*130), y=220+yPos, anchor="nw", width=100, height=30)
 
 def affichageValeursAnnées(canvas,fenetre,dataset,yPos):
   canvas.create_text(150,90+yPos,fill="black",font="Times 15 bold",text="Valeurs annuels")
   canvas.create_text(150,120+yPos,fill="black",font="Times 13",text="Min : "+str("{:.2f}".format(getMinAnnee(dataset))))
   canvas.create_text(150,140+yPos,fill="black",font="Times 13",text="Max : "+str("{:.2f}".format(getMaxAnnee(dataset))))
+  canvas.create_text(150,160+yPos,fill="black",font="Times 13",text="Score : "+str("{:.2f}".format(getScore(values_by_month,dataset)[0])))
   b = Button(fenetre, text="Graphique année", width=10, command=partial(getCourbeAnnee,dataset),background="white",foreground="black",activebackground="grey",activeforeground="black")
-  b.place(x=100, y=160+yPos, anchor="nw", width=100, height=30)
+  b.place(x=100, y=180+yPos, anchor="nw", width=100, height=30)
   affichageMois(canvas,fenetre,dataset,yPos)
 
 def affichageFenetre():
   fenetre = Tk()
   fenetre.title("Qualité des données")
-  fenetre.geometry('1800x800')
-  canvas=Canvas(fenetre,bg='#FFFFFF',width=800,height=800,scrollregion=(0,0,1500,1500))
-  canvas.create_text(900,20,fill="black",font="Times 20 bold",text="Trouver la capitale")
+  fenetre.geometry('1800x1000')
+  canvas=Canvas(fenetre,bg='#FFFFFF',width=800,height=1000,scrollregion=(0,0,1500,1500))
+  
+  b = Button(fenetre, text="Comparatif années", width=10, command=getAllCourbesAnnee,background="white",foreground="black",activebackground="grey",activeforeground="black")
+  b.place(x=800, y=20, anchor="nw", width=200, height=30)
   canvas.create_text(900,60,fill="red",font="Times 20 bold",text="Données propres")
   # Tableau propre
 
   affichageValeursAnnées(canvas,fenetre,values_by_month,0)
 
   # Tableau erreurs
-  canvas.create_text(900,250,fill="red",font="Times 20 bold",text="Données erreurs")
+  canvas.create_text(900,265,fill="red",font="Times 20 bold",text="Données erreurs")
   affichageValeursAnnées(canvas,fenetre,values_correction,200)
 
   # Tableau savukoski
-  canvas.create_text(900,450,fill="red",font="Times 20 bold",text="Données Savukoski kirkonkylä")
+  canvas.create_text(900,465,fill="red",font="Times 20 bold",text="Données Savukoski kirkonkylä")
   affichageValeursAnnées(canvas,fenetre,data_savukoski,400)
+
+
+  # Tableau helsinki
+  canvas.create_text(900,665,fill="red",font="Times 20 bold",text="Données d'Helsinki")
+  affichageValeursAnnées(canvas,fenetre,data_helsinki,600)
 
   canvas.pack(side=LEFT,expand=True,fill=BOTH)
   fenetre.mainloop()
@@ -245,8 +337,13 @@ values_correction = getValeursMoisErreurCorrection(setValeursMoisErreur(error))
 # Values opendata
 #
 savukoski = pd.read_excel('data/Savukoski_kirkonkyla.xlsx', sheet_name=2)
-data_savukoski=setValeursMoissavukoski(savukoski)
+data_savukoski=getValeursMoissavukoski(savukoski)
 
+#
+# Values Helsinki
+#
+helsinki = pd.read_excel('data/Helsinki.xlsx',sheet_name=0)
+data_helsinki=getValeursMoisHelsinki(helsinki)
 #
 # Affichage
 #
